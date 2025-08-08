66f4c441cc41a6242964e689e3652fbe186d9b37
abolfazl hosnian <abolfazlhosnian@gmail.com>
2025-08-08 12:05:21 +0330

    PoolsManager-Comments added

--- Assets/ir.soor.pooler/Scripts/Runtime/PoolsManager.cs
+++ Assets/ir.soor.pooler/Scripts/Runtime/PoolsManager.cs
@@ -7,15 +7,38 @@ namespace Soor.Pooler
 {
     public class PoolsManager : MonoBehaviour
     {
+        #region SERIALIZED_FIELDS
+
+        /// <summary>
+        /// If true, all <see cref="Pooler"/> instances in the `_allPoolers` list will be generated in the Awake method.
+        /// </summary>
         [SerializeField] private bool _generateAllPoolsOnAwake = false;
+        
+        /// <summary>
+        /// A list of all <see cref="Pooler"/> instances that this component manages.
+        /// </summary>
         [SerializeField] private List<Pooler> _allPoolers = new List<Pooler>();
+
+        #endregion SERIALIZED_FIELDS
+
+
+        #region METHODS
 
         private void Awake()
         {
             if (_generateAllPoolsOnAwake) _allPoolers.ForEach(GenerateObjectPool);
         }
 
-        public void AddPooler(string poolName, List<Poolable> objectsToPool, int poolDefaultCapacity = 10, int poolMaxCapecity = 1000, bool generatePoolImmediately = true)
+        /// <summary>
+        /// Adds a new <see cref="Pooler"/> instance to the list of managed pools.
+        /// </summary>
+        /// <param name="poolName">The unique name for the new pool.</param>
+        /// <param name="objectsToPool">The list of poolable prefabs to be pooled.</param>
+        /// <param name="poolDefaultCapacity">The default initial capacity of the pool.</param>
+        /// <param name="poolMaxCapacity">The maximum number of objects the pool can hold.</param>
+        /// <param name="generatePoolImmediately">If true, the ObjectPool will be generated immediately after the Pooler is added.</param>
+        /// <exception cref="ArgumentException">Thrown if a pool with the specified name already exists.</exception>
+        public void AddPooler(string poolName, List<Poolable> objectsToPool, int poolDefaultCapacity = 10, int poolMaxCapacity = 1000, bool generatePoolImmediately = true)
         {
             if (_allPoolers.Any(p => p.PoolName == poolName))
             {
@@ -23,21 +46,36 @@ namespace Soor.Pooler
                 throw new Exception("Already exists.");
             }
         
-            _allPoolers.Add(new Pooler(poolName, objectsToPool, poolDefaultCapacity, poolMaxCapecity));
+            _allPoolers.Add(new Pooler(poolName, objectsToPool, poolDefaultCapacity, poolMaxCapacity));
             if (generatePoolImmediately) GenerateObjectPool(_allPoolers[^1]);
         }
 
+        /// <summary>
+        /// Generates the <see cref="ObjectPool{T}"/> for the specified <see cref="Pooler"/> instance,
+        /// if it has not been generated already.
+        /// </summary>
+        /// <param name="pooler">The <see cref="Pooler"/> instance for which to generate the pool.</param>
         public void GenerateObjectPool(Pooler pooler)
         {
             if (pooler.ObjectPool == null) pooler.GenerateObjectPool();
         }
 
+        /// <summary>
+        /// Finds and generates the <see cref="ObjectPool{T}"/> for the <see cref="Pooler"/> with the specified name.
+        /// </summary>
+        /// <param name="name">The unique name of the pool to generate.</param>
         public void GenerateObjectPool(string name)
         {
             var intendedPooler = GetPooler(name);
             GenerateObjectPool(intendedPooler);
         }
 
+        /// <summary>
+        /// Retrieves a <see cref="Pooler"/> instance by its unique name.
+        /// </summary>
+        /// <param name="poolerName">The unique name of the pool to retrieve.</param>
+        /// <returns>The <see cref="Pooler"/> instance with the specified name.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown if a pool with the specified name does not exist.</exception>
         public Pooler GetPooler(string poolerName)
         {
             var intendedPooler = _allPoolers.FirstOrDefault(p => p.PoolName == poolerName);
@@ -46,15 +84,25 @@ namespace Soor.Pooler
             throw new Exception("Not found");
         }
 
+        /// <summary>
+        /// Finds and destroys the pool with the specified name, including all its GameObjects.
+        /// </summary>
+        /// <param name="name">The unique name of the pool to destroy.</param>
         public void DestroyObjectPool(string name)
         {
             var intendedPooler = GetPooler(name);
             DestroyObjectPool(intendedPooler);
         }
 
+        /// <summary>
+        /// Destroys a pooler and its GameObjects.
+        /// </summary>
+        /// <param name="pooler">The pooler instance to destroy.</param>
         public void DestroyObjectPool(Pooler pooler)
         {
             pooler.DestroyObjectPool();
         }
+        
+        #endregion METHODS
     }
 }
